ec1bcc03d6c1128aee86564c6b336ae4d938f78d
Thomas123414 <139514581+Thomas123414@users.noreply.github.com>
2024-01-04 00:16:29 +1100

    Better Time-lapse on website
    Updated the HTML, CSS and JS to make a better Time-Lapse clock then the old one

--- index.js
+++ index.js
@@ -1,57 +1,29 @@
-let hour = 00; 
-let minute = 00; 
-let second = 00; 
-let count = 00;
-let timer;
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
 
-function stopWatch() { 
-	timer = true;
-    if (timer) { 
-		count++; 
-
-		if (count == 100) { 
-			second++; 
-			count = 0; 
-		} 
-
-		if (second == 60) { 
-			minute++; 
-			second = 0; 
-		} 
-
-		if (minute == 60) { 
-			hour++; 
-			minute = 0; 
-			second = 0; 
-		} 
-
-		let hrString = hour; 
-		let minString = minute; 
-		let secString = second; 
-		let countString = count; 
-
-		if (hour < 10) { 
-			hrString = "0" + hrString; 
-		} 
-
-		if (minute < 10) { 
-			minString = "0" + minString; 
-		} 
-
-		if (second < 10) { 
-			secString = "0" + secString; 
-		} 
-
-		if (count < 10) { 
-			countString = "0" + countString; 
-		} 
-
-		document.getElementById('hr').innerHTML = hrString; 
-		document.getElementById('min').innerHTML = minString; 
-		document.getElementById('sec').innerHTML = secString; 
-		document.getElementById('count').innerHTML = countString; 
-		setTimeout(stopWatch, 8); 
-	} 
+function start(){
+	startTime = Date.now() - elapsedTime;
+	timer = setInterval(update, 10);
 }
 
-Window.onload = stopWatch();
+function update(){
+
+	const currentTime = Date.now();
+	elapsedTime = currentTime - startTime;
+	
+	let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+	let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+	let seconds = Math.floor(elapsedTime / 1000 % 60);
+	let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+	hours = String(hours).padStart(2, "0");
+	minutes = String(minutes).padStart(2, "0");
+	seconds = String(seconds).padStart(2, "0");
+	milliseconds = String(milliseconds).padStart(2, "0");
+
+	display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
+
+Window.onload = start();
